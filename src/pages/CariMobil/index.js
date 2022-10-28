9832a6e214a1f3e170f202ce511d550aad647014
RayhanFachreza <berceritacerita0@gmail.com>
2022-10-28 12:24:42 +0700

    consume beck end data

--- src/pages/CariMobil/index.js
+++ src/pages/CariMobil/index.js
@@ -4,11 +4,54 @@ import "./style.css";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 // import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const CariMobil = () => {
   const [visible, setVisible] = useState(false);
+
+  const [cars, setCars] = useState([]);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [minPrice, setMinPrice] = useState(null);
+  const [maxPrice, setMaxPrice] = useState(null);
+  // const [price, setPrice] = useState(null);
+  const [isRented, setIsRented] = useState(false);
+
+  const handlePrice = (e) => {
+    const val = e.target.value;
+    if (val === "1") {
+      setMinPrice(0);
+      setMaxPrice(399999);
+    }
+    else if (val === "2") {
+      setMinPrice(400000);
+      setMaxPrice(600000);
+    }
+    else if (val === "3") {
+      setMinPrice(600001);
+      setMaxPrice(2000000);
+    }
+  };
+  const handleSubmit = () => {
+    const params = {
+      name,
+      minPrice,
+      maxPrice,
+      isRented: Boolean(isRented),
+      category,
+      page: 1,
+      pageSize: 9 ,
+    };
+
+    axios.get(`${process.env.REACT_APP_API_URL}/customer/v2/car`, { params })
+      .then((res) => {
+        setCars(res.data.cars);
+        setVisible(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <section className="carimobil">
@@ -32,6 +75,7 @@ const CariMobil = () => {
                     size="sm"
                     type="text"
                     placeholder="Ketik nama/tipe mobil"
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
               </div>
@@ -41,11 +85,12 @@ const CariMobil = () => {
                   <Form.Select
                     aria-label="Default select example"
                     className="selecttext"
+                    onChange={(e) => setCategory(e.target.value)}
                   >
                     <option hidden>Masukkan Kapasitas Mobil </option>
-                    <option value="1"> 2 - 4 Orang </option>
-                    <option value="2"> 4 - 6 Orang </option>
-                    <option value="3"> 6 - 8 Orang </option>
+                    <option value="small"> 2 - 4 Orang </option>
+                    <option value="medium"> 4 - 6 Orang </option>
+                    <option value="large"> 6 - 8 Orang </option>
                   </Form.Select>
                 </div>
               </div>
@@ -58,11 +103,12 @@ const CariMobil = () => {
                   <Form.Select
                     aria-label="Default select example"
                     className="selecttext"
+                    onChange={(e) => handlePrice(e)}
                   >
                     <option hidden>Masukkan Harga Sewa per Hari </option>
-                    <option value="1"> Rp. 400.000 </option>
+                    <option value="1"> {"< Rp. 400.000"} </option>
                     <option value="2"> Rp. 400.000 - Rp. 600.000</option>
-                    <option value="3"> Rp. 400.000 </option>
+                    <option value="3"> {"> Rp. 600.000 "}</option>
                   </Form.Select>
                 </div>
               </div>
@@ -75,18 +121,42 @@ const CariMobil = () => {
                       className="selecttext"
                     >
                       <option hidden>Status </option>
-                      <option value="1"> Rp. 400.000 </option>
-                      <option value="2"> Rp. 400.000 - Rp. 600.000</option>
-                      <option value="3"> Rp. 400.000 </option>
+                      <option value="true"> Disewa </option>
+                      <option value="false"> Ready </option>
                     </Form.Select>
                   </div>
-                  <Link to="/hasil-pencarian" className="tombolcari">
-                    <Button>Cari Mobil</Button>
-                  </Link>
+                  <div className="tombolcari">
+                    <Button onClick={() => handleSubmit()}>Cari Mobil</Button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        {/*  */}
+        <div className="row cardsection g-4">
+          {cars.length > 0 &&
+            cars.map((car) => (
+              <div className="col-lg-4 col-xl-4 col-sm-12 col-md-6 ">
+                <div className="card p-3">
+                  <img src={car.image} className="imagecar" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{car.name}</h5>
+                    <h4 className="harga">
+                      
+                      {`${car.price.toLocaleString("id-ID", {style:"currency", currency:"IDR"})}  / hari`}
+
+                      </h4>
+                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At nostrum eveniet culpa rerum, non dolorem reiciendis recusandae vero. Esse, maiores?</p>
+                    <div className="d-grid gap-2">
+                      <button className="btn btn-" type="button">
+                        Pilih Mobil
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </section>
